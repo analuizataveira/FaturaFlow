38015c3ed8f60c5f8996724f60ad828706ca3880
Ana Luiza <106170960+analuizataveira@users.noreply.github.com>
2025-05-01 15:55:11 -0300

    fix :hammer: pipeline

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,8 +1,12 @@
-const { defineConfig } = require('cypress');
+ 
+import { defineConfig } from 'cypress'
 
-
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 })
